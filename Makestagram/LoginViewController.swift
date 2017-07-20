9aa1cf1a60f8a00f0bc06bfecb81ebeefb27f5b9
selimbenlamine <30296030+selimbenlamine@users.noreply.github.com>
2017-07-20 14:45:42 -0400

    update login page

--- Makestagram/LoginViewController.swift
+++ Makestagram/LoginViewController.swift
@@ -11,15 +11,22 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var loginButton: UIButton!
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        print("login button tapped")
+
+    }
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+// override func didReceiveMemoryWarning() {
+    //   super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    //}
     
     
 }

8cd9f9fb17e16518ed2e9088478ae2050d52bef2
selimbenlamine <30296030+selimbenlamine@users.noreply.github.com>
2017-07-20 16:38:21 -0400

    Update

--- Makestagram/AppDelegate.swift
+++ Makestagram/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
+        
         let storyboard = UIStoryboard(name: "Login", bundle: .main)
         
         // 2
--- Makestagram/LoginViewController.swift
+++ Makestagram/LoginViewController.swift
@@ -6,16 +6,30 @@
 //  Copyright © 2017 Selim Ben Lamine. All rights reserved.
 //
 
-import Foundation
 import UIKit
+import FirebaseAuth
+import FirebaseAuthUI
+import FirebaseDatabase
+
+typealias FIRUser = FirebaseAuth.User
 
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var loginButton: UIButton!
-    @IBAction func loginButtonTapped(_ sender: Any) {
-        print("login button tapped")
-
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        
+        guard let authUI = FUIAuth.defaultAuthUI()
+            else { return }
+        // 2
+        authUI.delegate = self
+        
+        // 3
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
     }
+
+    
    
     
     override func viewDidLoad() {
@@ -28,6 +42,30 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     //}
     
-    
 }
 
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        
+        // 1
+        guard let user = user
+            else { return }
+        
+        // 2
+        let userRef = Database.database().reference().child("users").child(user.uid)
+        
+        // 3
+        userRef.observeSingleEvent(of: .value, with: { (snapshot) in
+            // 4 retrieve user data from snapshot
+            if let userDict = snapshot.value as? [String : Any] {
+                print("User already exists \(userDict.debugDescription).")
+            } else {
+                print("New user!")
+            }
+        })
+    }
+}
